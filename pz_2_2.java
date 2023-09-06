e75efcaf856eda3c58361382d79b547ee5853290
Quland0 <114486014+Quland0@users.noreply.github.com>
2023-09-06 22:45:15 +0300

    Add files via upload

---
+++ pz_2_2.java
@@ -0,0 +1,12 @@
+public class pz_2_2 {
+        public static void main(String[] args){
+            //System.out.println("Я прекрасно понимаю разницу ");
+            System.out.print("Я прекрасно понимаю разницу ");
+            System.out.println("между System.out.println()");
+            //System.out.print("между System.out.println()");
+            //System.out.println("и System.out.print(). ");
+            System.out.print("и System.out.print(). ");
+            //.out.println("А ты?");
+            System.out.print("А ты?");
+        }
+}
---
+++ pz_3.java
@@ -0,0 +1,6 @@
+class pz_3{
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.print("Programming!");
+    }
+}
