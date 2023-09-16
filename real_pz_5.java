93565d2e6608aaa114341826e97f2029c3868337
Quland0 <114486014+Quland0@users.noreply.github.com>
2023-09-16 15:42:39 +0300

    Update real_pz_5.java

--- real_pz_5.java
+++ real_pz_5.java
@@ -1,3 +1,4 @@
+1.
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -26,7 +27,7 @@ public class Main{
 
 	
 }
-
+2.
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -59,4 +60,39 @@ public class Main{
         
     }
 	
+}
+
+4.
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Кол-во человек в строю: ");
+        
+        int n = scanner.nextInt();
+        int[] heights = new int[n];
+
+        System.out.println("рост каждого в строю: ");
+        for (int i = 0; i < n; i++) {
+            heights[i] = scanner.nextInt();
+        }
+
+        System.out.println("рост Пети: ");
+        int petyasHeight = scanner.nextInt();
+
+        int position = 1;
+        // while (position <= n && heights[position - 1] > petyasHeight) {
+        //     position++;
+        // }
+        for(int i = 0; i < n; i++){
+            if (petyasHeight <= heights[i]){
+                position++;
+            }
+        }
+
+        System.out.println("Петя должен встать на позицию: " + position);
+    }
 }
